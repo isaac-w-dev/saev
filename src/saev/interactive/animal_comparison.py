7e7e4fcedcf8c691764651500b2bc628976432c4
Isaac <igwilson99@gmail.com>
2025-08-01 14:55:05 -0400

    Rearranged marimo app for better organization

--- src/saev/interactive/animal_comparison.py
+++ src/saev/interactive/animal_comparison.py
@@ -16,19 +16,99 @@ def _():
     import polars as pl
     import torch
     import tqdm
+    import json
     return json, mo, os
 
 
 @app.cell
 def _(mo):
-    mo.md(r"""Enter the directory of the the sorted taxon dataset""")
-    return
+    def create_label(file_location):
+        label = open(file_location).read().strip()
+        label = label.split('_')
+        label = ' '.join(label[1:])
+        return mo.md(label)
+    return (create_label,)
 
 
 @app.cell
-def _():
-    taxon_directory = '/local/scratch/wilson.5057/taxons'
-    return (taxon_directory,)
+def _(mo):
+    def display_image(image_location):
+        return mo.image(src=image_location, width=200, rounded=True)
+    return (display_image,)
+
+
+@app.cell
+def _(json):
+    def display_metadata(metadata_location):
+        with open(metadata_location) as metadata:
+            data = json.load(metadata)
+            return data
+    return (display_metadata,)
+
+
+@app.cell
+def _(create_label, display_image, display_metadata, mo, os):
+    def compile_info(species_instance_directory):
+        info_block_array = []
+        for folder in os.listdir(species_instance_directory):
+            species_instance = species_instance_directory + '/' + folder
+            info_block = ['Error', 'Error', 'Error', 'Error']
+            for file in os.listdir(species_instance):
+                file_location = species_instance + '/' + file
+                if file[-4:] == '.png':
+                    info_block[1] = display_image(file_location)
+                if file[-4:] == '.txt':
+                    info_block[0] = create_label(file_location)
+                if file[-5:] == '.json':
+                    checkpoint = mo.md(file[-22:-14])
+                    info_block[2] = checkpoint
+                    info_block[3] = display_metadata(file_location)
+            info_dict = {'Name':info_block[0], 'Photo':info_block[1], 'Checkpoint':info_block[2], 'Neuron':info_block[3]['neuron'], 'Frequency' : 10 ** info_block[3]['log10_freq'], 'Value': 10 ** info_block[3]['log10_value']}
+            info_block_array.append(info_dict)
+
+        return mo.hstack(info_block_array, widths='equal', justify='start')
+    return (compile_info,)
+
+
+@app.cell
+def _(os):
+    def compile_species(directory):
+        count = 0
+        Genus_paths = []
+        for Kingdom_directory in os.listdir(directory):
+            Kingdom_path = directory + '/' + Kingdom_directory
+            Kingdom = os.listdir(Kingdom_path)
+            for Phylum_directory in Kingdom:
+                Phylum_path = Kingdom_path + '/' + Phylum_directory
+                Phylum = os.listdir(Phylum_path)
+                for Class_directory in Phylum:
+                    Class_path = Phylum_path + '/' + Class_directory
+                    Class = os.listdir(Class_path)
+                    for Order_directory in Class:
+                        Order_path = Class_path + '/' + Order_directory
+                        Order = os.listdir(Order_path)
+                        for Family_directory in Order:
+                            Family_path = Order_path + '/' + Family_directory
+                            Family = os.listdir(Family_path)
+                            for Genus_directory in Family:
+                                Genus_path = Family_path + '/' + Genus_directory
+                                Genus_paths.append(Genus_path)
+                                count += 1
+        return count - 1, Genus_paths
+
+    return (compile_species,)
+
+
+@app.cell
+def _(compile_info, mo, os):
+    def display_by_genus(genus_directory, get_i):
+        genus_grid = []
+        directory = genus_directory[get_i()]
+        for species in os.listdir(directory):
+            species_path = directory + '/' + species
+            genus_grid.append(compile_info(species_path))
+        return mo.vstack([genus_grid], heights='equal', justify='start')
+    return (display_by_genus,)
 
 
 @app.cell
@@ -45,6 +125,18 @@ def _(mo, os):
 
 
 @app.cell
+def _(mo):
+    mo.md(r"""Enter the directory of the the sorted taxon dataset""")
+    return
+
+
+@app.cell
+def _():
+    taxon_directory = '/local/scratch/wilson.5057/taxons'
+    return (taxon_directory,)
+
+
+@app.cell
 def _(make_dropdown, taxon_directory):
     Kingdom = make_dropdown(taxon_directory, 'Kingdom')
     return (Kingdom,)
@@ -54,12 +146,6 @@ def _(make_dropdown, taxon_directory):
 def _(Kingdom, mo):
     mo.hstack([Kingdom], justify="start")
     return
-
-
-@app.cell
-def _(mo):
-    get_i, set_i = mo.state(0)
-    return get_i, set_i
 
 
 @app.cell
@@ -148,68 +234,13 @@ def _(Species, Species_directory):
 def _(Species_Instance_directory, compile_info, mo):
     species = compile_info(Species_Instance_directory)
     mo.vstack([species], heights='equal', justify='start')
-
-
     return
 
 
 @app.cell
 def _(mo):
-    def create_label(file_location):
-        label = open(file_location).read().strip()
-        label = label.split('_')
-        label = ' '.join(label[1:])
-        return mo.md(label)
-
-    return (create_label,)
-
-
-@app.cell
-def _(mo):
-    def display_image(image_location):
-        return mo.image(src=image_location, width=200, rounded=True)
-    return (display_image,)
-
-
-@app.cell
-def _(json):
-    def display_metadata(metadata_location):
-        with open(metadata_location) as metadata:
-            data = json.load(metadata)
-            # data['neuron']
-            # data['log10_freq']
-            # data['log10_value']
-            return data
-    return (display_metadata,)
-
-
-@app.cell
-def _(os):
-    def compile_species(directory):
-        count = 0
-        Genus_paths = []
-        for Kingdom_directory in os.listdir(directory):
-            Kingdom_path = directory + '/' + Kingdom_directory
-            Kingdom = os.listdir(Kingdom_path)
-            for Phylum_directory in Kingdom:
-                Phylum_path = Kingdom_path + '/' + Phylum_directory
-                Phylum = os.listdir(Phylum_path)
-                for Class_directory in Phylum:
-                    Class_path = Phylum_path + '/' + Class_directory
-                    Class = os.listdir(Class_path)
-                    for Order_directory in Class:
-                        Order_path = Class_path + '/' + Order_directory
-                        Order = os.listdir(Order_path)
-                        for Family_directory in Order:
-                            Family_path = Order_path + '/' + Family_directory
-                            Family = os.listdir(Family_path)
-                            for Genus_directory in Family:
-                                Genus_path = Family_path + '/' + Genus_directory
-                                Genus_paths.append(Genus_path)
-                                count += 1
-        return count - 1, Genus_paths
-
-    return (compile_species,)
+    get_i, set_i = mo.state(0)
+    return get_i, set_i
 
 
 @app.cell
@@ -220,51 +251,44 @@ def _(compile_species, get_i, mo, set_i, taxon_directory):
 
 
 @app.cell
-def _(create_label, display_image, display_metadata, mo, os):
-    def compile_info(species_instance_directory):
-        info_block_array = []
-        for folder in os.listdir(species_instance_directory):
-            species_instance = species_instance_directory + '/' + folder
-            info_block = ['Error', 'Error', 'Error', 'Error']
-            for file in os.listdir(species_instance):
-                file_location = species_instance + '/' + file
-                if file[-4:] == '.png':
-                    info_block[1] = (display_image(file_location))
-                if file[-4:] == '.txt':
-                    info_block[0] = create_label(file_location)
-                if file[-5:] == '.json':
-                    checkpoint = mo.md(file[-22:-14])
-                    info_block[2] = checkpoint
-                    info_block[3] = display_metadata(file_location)
-            info_block_array.append(info_block)
-        return mo.hstack(info_block_array, widths='equal', justify='start')
-    return (compile_info,)
-
-
-@app.cell
-def _(compile_info, genus_paths, get_i, mo, os):
-    def display_by_genus(genus_directory, get_i):
-        genus_grid = []
-        directory = genus_directory[get_i()]
-        for species in os.listdir(directory):
-            species_path = directory + '/' + species
-            genus_grid.append(compile_info(species_path))
-            # for species_instance in os.listdir(species_path):
-            #     species_instance_path = species_path + '/' + species_instance
-            #     print(species_instance_path)
-                # genus_grid.append(compile_info(species_instance_path))
-        return mo.vstack([genus_grid], heights='equal', justify='start')
-
+def _(display_by_genus, genus_paths, get_i):
     grid = display_by_genus(genus_paths, get_i)
     return (grid,)
 
 
 @app.cell
-def _(genus_slider, grid, mo):
+def _(mo):
+    sort_by_freq_btn = mo.ui.run_button(label="Sort by frequency")
+    sort_by_value_btn = mo.ui.run_button(label="Sort by value")
+    sort_by_neuron_btn = mo.ui.run_button(label="Sort by neuron")
+    return sort_by_freq_btn, sort_by_neuron_btn, sort_by_value_btn
+
+
+@app.cell
+def _(grid, sort_by_freq_btn, sort_by_neuron_btn, sort_by_value_btn):
+    if sort_by_neuron_btn.value:
+        new_grid = sorted(grid, key=lambda dct: int(dct["Neuron"]))
+    elif sort_by_freq_btn.value:
+        new_grid = sorted(grid, key=lambda dct: float(dct["Frequency"]))
+    elif sort_by_value_btn.value:
+        new_grid = sorted(grid, key=lambda dct: float(dct["Value"]))
+    else:
+        new_grid = grid
+    return (new_grid,)
+
+
+@app.cell
+def _(mo, sort_by_freq_btn, sort_by_neuron_btn, sort_by_value_btn):
+    mo.hstack([sort_by_freq_btn, sort_by_value_btn, sort_by_neuron_btn], justify="start")
+    return
+
+
+@app.cell
+def _(genus_slider, mo, new_grid):
     mo.md(
         f"""
     {genus_slider}
-    {grid}
+    {new_grid}
     """
     )
     return
