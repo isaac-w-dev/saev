f847d9b764cb81cc52a95bdc667706536ed1d403
Isaac <igwilson99@gmail.com>
2025-08-07 16:42:18 -0400

    Fully implemented options to sort by Neuron, Log10 Frequency, and Log10 Value. As well as a forward and back button to easily swap between genuses.

--- src/saev/interactive/animal_comparison.py
+++ src/saev/interactive/animal_comparison.py
@@ -20,54 +20,75 @@ def _():
     return json, mo, os
 
 
-@app.cell
-def _(mo):
-    def create_label(file_location):
-        label = open(file_location).read().strip()
-        label = label.split('_')
-        label = ' '.join(label[1:])
-        return mo.md(label)
-    return (create_label,)
+@app.function
+def extract_taxonomy(file_location):
+    label = open(file_location).read().strip()
+    label = label.split('_')
+    label = ' '.join(label[1:])
+    return label
 
 
 @app.cell
 def _(mo):
     def display_image(image_location):
         return mo.image(src=image_location, width=200, rounded=True)
-    return (display_image,)
+    return
 
 
 @app.cell
 def _(json):
-    def display_metadata(metadata_location):
+    def extract_metadata(metadata_location):
         with open(metadata_location) as metadata:
             data = json.load(metadata)
             return data
-    return (display_metadata,)
+    return (extract_metadata,)
 
 
 @app.cell
-def _(create_label, display_image, display_metadata, mo, os):
+def _(extract_metadata, os):
     def compile_info(species_instance_directory):
         info_block_array = []
         for folder in os.listdir(species_instance_directory):
             species_instance = species_instance_directory + '/' + folder
-            info_block = ['Error', 'Error', 'Error', 'Error']
+            info_block = ['Error', 'Error', 'Error', 'Error', 'Error', 'Error']
             for file in os.listdir(species_instance):
                 file_location = species_instance + '/' + file
-                if file[-4:] == '.png':
-                    info_block[1] = display_image(file_location)
                 if file[-4:] == '.txt':
-                    info_block[0] = create_label(file_location)
+                    info_block[0] = extract_taxonomy(file_location)
+                if file[-4:] == '.png':
+                    info_block[1] = file_location
                 if file[-5:] == '.json':
-                    checkpoint = mo.md(file[-22:-14])
+                    checkpoint = file[-22:-14]
                     info_block[2] = checkpoint
-                    info_block[3] = display_metadata(file_location)
-            info_dict = {'Name':info_block[0], 'Photo':info_block[1], 'Checkpoint':info_block[2], 'Neuron':info_block[3]['neuron'], 'Frequency' : 10 ** info_block[3]['log10_freq'], 'Value': 10 ** info_block[3]['log10_value']}
-            info_block_array.append(info_dict)
-
-        return mo.hstack(info_block_array, widths='equal', justify='start')
+                    metadata =  extract_metadata(file_location)
+                    info_block[3] = metadata['neuron']
+                    info_block[4] = metadata['log10_freq']
+                    info_block[5] = metadata['log10_value']
+            # info_dict = {'Name':info_block[0], 'Photo':info_block[1], 'Checkpoint':info_block[2], 'Neuron':info_block[3]['neuron'], 'Frequency' : 10 ** info_block[3]
+            #     ['log10_freq'], 'Value': 10 ** info_block[3]['log10_value']}
+            info_block_array.append(info_block)
+        return info_block_array
     return (compile_info,)
+
+
+@app.cell
+def _(mo):
+    def adjust_content_display(grid):
+        adjusted_grid = []
+        for list in grid:
+            new_list = []
+            for element in list:
+                item = []
+                item.append(mo.md(f'Name: {element[0]}'))
+                item.append(mo.image(src=element[1], width=200, rounded=True))
+                item.append(mo.md(f'Checkpoint: {element[2]}'))
+                item.append(mo.md(f'Neuron: {element[3]}'))
+                item.append(mo.md(f'Log10 Frequency: {element[4]}'))
+                item.append(mo.md(f'Log10 Value: {element[5]}'))
+                new_list.append(item)
+            adjusted_grid.append(mo.hstack(new_list))
+        return mo.vstack(adjusted_grid)
+    return (adjust_content_display,)
 
 
 @app.cell
@@ -100,14 +121,14 @@ def _(os):
 
 
 @app.cell
-def _(compile_info, mo, os):
+def _(compile_info, os):
     def display_by_genus(genus_directory, get_i):
         genus_grid = []
         directory = genus_directory[get_i()]
         for species in os.listdir(directory):
             species_path = directory + '/' + species
             genus_grid.append(compile_info(species_path))
-        return mo.vstack([genus_grid], heights='equal', justify='start')
+        return genus_grid
     return (display_by_genus,)
 
 
@@ -121,119 +142,6 @@ def _(mo, os):
         except FileNotFoundError:
             choices = []
         return mo.ui.dropdown(choices, label=rank)
-    return (make_dropdown,)
-
-
-@app.cell
-def _(mo):
-    mo.md(r"""Enter the directory of the the sorted taxon dataset""")
-    return
-
-
-@app.cell
-def _():
-    taxon_directory = '/local/scratch/wilson.5057/taxons'
-    return (taxon_directory,)
-
-
-@app.cell
-def _(make_dropdown, taxon_directory):
-    Kingdom = make_dropdown(taxon_directory, 'Kingdom')
-    return (Kingdom,)
-
-
-@app.cell
-def _(Kingdom, mo):
-    mo.hstack([Kingdom], justify="start")
-    return
-
-
-@app.cell
-def _(Kingdom, make_dropdown, mo, taxon_directory):
-    try:
-        Phylum_directory = taxon_directory + '/' + Kingdom.value
-        Phylum = make_dropdown(Phylum_directory, 'Phylum')
-    except:
-        print("Kingdom has not been set. Select Kingdom to continue")
-
-    mo.hstack([Phylum], justify="start")
-
-    return Phylum, Phylum_directory
-
-
-@app.cell
-def _(Phylum, Phylum_directory, make_dropdown, mo):
-    try:
-        Class_directory = Phylum_directory + '/' + Phylum.value
-        Class = make_dropdown(Class_directory, 'Class')
-    except:
-        print("Phylum has not been set. Select Phylum to continue")
-
-    mo.hstack([Class], justify="start")
-    return Class, Class_directory
-
-
-@app.cell
-def _(Class, Class_directory, make_dropdown, mo):
-    try:
-        Order_directory = Class_directory + '/' + Class.value
-        Order = make_dropdown(Order_directory, 'Order')
-    except:
-        print("Class has not been set. Select Class to continue")
-
-    mo.hstack([Order], justify="start")
-    return Order, Order_directory
-
-
-@app.cell
-def _(Order, Order_directory, make_dropdown, mo):
-    try:
-        Family_directory = Order_directory + '/' + Order.value
-        Family = make_dropdown(Family_directory, 'Family')
-    except:
-        print("Order has not been set. Select Order to continue")
-
-    mo.hstack([Family], justify="start")
-    return Family, Family_directory
-
-
-@app.cell
-def _(Family, Family_directory, make_dropdown, mo):
-    try:
-        Genus_directory = Family_directory + '/' + Family.value
-        Genus = make_dropdown(Genus_directory, 'Genus')
-    except:
-        print("Family has not been set. Select Family to continue")
-
-    mo.hstack([Genus], justify="start")
-    return Genus, Genus_directory
-
-
-@app.cell
-def _(Genus, Genus_directory, make_dropdown, mo):
-    try:
-        Species_directory = Genus_directory + '/' + Genus.value
-        Species = make_dropdown(Species_directory, 'Species')
-    except:
-        print("Genus has not been set. Select Genus to continue")
-
-    mo.hstack([Species], justify="start")
-    return Species, Species_directory
-
-
-@app.cell
-def _(Species, Species_directory):
-    try:
-        Species_Instance_directory = Species_directory + '/' + Species.value
-    except:
-        print("Species has not been set. Select Species to continue")
-    return (Species_Instance_directory,)
-
-
-@app.cell
-def _(Species_Instance_directory, compile_info, mo):
-    species = compile_info(Species_Instance_directory)
-    mo.vstack([species], heights='equal', justify='start')
     return
 
 
@@ -244,51 +152,89 @@ def _(mo):
 
 
 @app.cell
+def _():
+    taxon_directory = '/local/scratch/wilson.5057/taxons'
+    return (taxon_directory,)
+
+
+@app.cell
 def _(compile_species, get_i, mo, set_i, taxon_directory):
     genus_count, genus_paths = compile_species(taxon_directory)
-    genus_slider = mo.ui.slider(0, genus_count, value=get_i(), on_change=lambda i: set_i(i), full_width=True,)
-    return genus_paths, genus_slider
+    genus_slider = mo.ui.slider(0, genus_count, value=get_i(), on_change=lambda i: set_i(int(i)), full_width=True,)
+    return genus_count, genus_paths, genus_slider
 
 
 @app.cell
 def _(display_by_genus, genus_paths, get_i):
     grid = display_by_genus(genus_paths, get_i)
+    print(grid)
     return (grid,)
 
 
 @app.cell
-def _(mo):
-    sort_by_freq_btn = mo.ui.run_button(label="Sort by frequency")
-    sort_by_value_btn = mo.ui.run_button(label="Sort by value")
+def _(genus_count, mo, set_i):
     sort_by_neuron_btn = mo.ui.run_button(label="Sort by neuron")
-    return sort_by_freq_btn, sort_by_neuron_btn, sort_by_value_btn
+    sort_by_freq_btn = mo.ui.run_button(label="Sort by Log10 Frequency")
+    sort_by_value_btn = mo.ui.run_button(label="Sort by Log10 Value")
+    prev_btn = mo.ui.run_button(label="Previous", 
+                               on_change=lambda  _: set_i(lambda g: (g - 1) % genus_count))
+    next_btn = mo.ui.run_button(label="Next",
+                               on_change=lambda  _: set_i(lambda g: (g + 1) % genus_count))
+    return (
+        next_btn,
+        prev_btn,
+        sort_by_freq_btn,
+        sort_by_neuron_btn,
+        sort_by_value_btn,
+    )
 
 
 @app.cell
 def _(grid, sort_by_freq_btn, sort_by_neuron_btn, sort_by_value_btn):
+    new_grid = []
     if sort_by_neuron_btn.value:
-        new_grid = sorted(grid, key=lambda dct: int(dct["Neuron"]))
+        for x in grid:
+            # Sorts items by the value of neuron from lowest to highest
+            new_grid.append(sorted(x, key=lambda list: list[3]))
     elif sort_by_freq_btn.value:
-        new_grid = sorted(grid, key=lambda dct: float(dct["Frequency"]))
+        for x in grid:
+            # Sorts items by the value of neuron from lowest to highest
+            new_grid.append(sorted(x, key=lambda list: list[4]))
     elif sort_by_value_btn.value:
-        new_grid = sorted(grid, key=lambda dct: float(dct["Value"]))
+        for x in grid:
+            # Sorts items by the value of neuron from lowest to highest
+            new_grid.append(sorted(x, key=lambda list: list[5]))
     else:
         new_grid = grid
     return (new_grid,)
 
 
 @app.cell
-def _(mo, sort_by_freq_btn, sort_by_neuron_btn, sort_by_value_btn):
-    mo.hstack([sort_by_freq_btn, sort_by_value_btn, sort_by_neuron_btn], justify="start")
+def _(adjust_content_display, new_grid):
+    restored_grid = adjust_content_display(new_grid)
+    return (restored_grid,)
+
+
+@app.cell
+def _(
+    mo,
+    next_btn,
+    prev_btn,
+    sort_by_freq_btn,
+    sort_by_neuron_btn,
+    sort_by_value_btn,
+):
+    mo.vstack([mo.hstack([sort_by_neuron_btn, sort_by_freq_btn, sort_by_value_btn], justify="start"), mo.hstack([prev_btn, next_btn], justify="start")])
+
     return
 
 
 @app.cell
-def _(genus_slider, mo, new_grid):
+def _(genus_slider, mo, restored_grid):
     mo.md(
         f"""
     {genus_slider}
-    {new_grid}
+    {restored_grid}
     """
     )
     return
